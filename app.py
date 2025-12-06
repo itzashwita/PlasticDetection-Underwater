046170ac6e7f3feef1e191c3b52aac169d5a4ed2
Ashwita Ramanavelan <itzashwita@gmail.com>
2025-12-06 08:25:17 -0600

    Fix model path string format in app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ if submitted:
 # -------------------------------
 # ✅ LOAD YOLO MODEL
 # -------------------------------
-MODEL_PATH = .\best.pt
+MODEL_PATH = "best.pt"
 model = YOLO(MODEL_PATH)
 
 # -------------------------------
